83f20af6536cd544f59f407b66a4e53a02839f73
Queverity <pryor.baird@ucas-edu.net>
2025-08-21 09:16:44 -0600

    yipeeeee

--- starters/seasons.py
+++ starters/seasons.py
@@ -1,4 +1,5 @@
-name = input("What is your name: ").capitalize()
-season = input("What is your favorite season: ").capitalize()
+# CB 1st Seasons
+last_name = input(f"What is your last name: ").capitalize()
+favorite_season = input(f"What is your favorite season: ").capitalize()
 
-print(f"Wow {name}, I like {season} too!")
+print(f"Wow {last_name} san, I like {favorite_season} too!")
