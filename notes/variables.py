a2c4e8e8978c6a3223f56d19ef5daacdc578d459
Queverity <pryor.baird@ucas-edu.net>
2025-08-21 09:52:26 -0600

    variables notes + who_are_you

---
+++ notes/variables.py
@@ -0,0 +1,11 @@
+# CB 1st Variable Notes
+
+even_number = 2
+grade = 100
+animal = "Panda"
+
+print(grade)
+
+grade = 95
+
+print(grade)
---
+++ practices/who_are_you.py
@@ -0,0 +1,5 @@
+name = input(f"What is your name?").capitalize()
+age = int(input(f"What is your age in years?"))
+favorite_color = input(f"What is your favorite color?")
+
+print(f"You are {name}, {age} years old, and your favorite color is {favorite_color}.")
