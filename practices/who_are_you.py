f53e3d33ebe6996366de41bd7c88e9e982facd07
Queverity <pryor.baird@ucas-edu.net>
2025-08-22 09:52:30 -0600

    Started working on Who are You

--- practices/who_are_you.py
+++
@@ -1,5 +0,0 @@
-name = input(f"What is your name?").capitalize()
-age = int(input(f"What is your age in years?"))
-favorite_color = input(f"What is your favorite color?")
-
-print(f"You are {name}, {age} years old, and your favorite color is {favorite_color}.")
