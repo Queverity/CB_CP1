b9ff98d2cb8d2a949e06fc6db0f65632d1e3fa0d
Queverity <pryor.baird@ucas-edu.net>
2025-08-27 09:53:02 -0600

    finished average grade program and took integer notes

---
+++ notes/integers.py
@@ -0,0 +1,38 @@
+# CB 1st Integers and Floats Notes
+
+# Integers
+
+age = 14
+year = 2025
+room = 200
+hour = 9
+
+# Floats
+
+pi = 3.14159
+percent = -.75
+damage = 9.75
+
+# Arithmatic
+print(5+2) #7
+print(5-2) #3
+print(5*2) #10
+print(5/2) #2.5
+print(5//2) #2
+print(5%2) #1
+print(5**2) #25
+
+print(5 + 2 * 9 + (5**5)%3)
+
+# Type Casting
+str_variable = "8"
+num_variable = int(str_variable)
+print(num_variable, type(num_variable))
+
+str_variable_two = "3.64"
+float_variable = float(str_variable_two)
+print(float_variable, type(float_variable))
+
+
+rounded_variable = round(float_variable, 1)
+print(rounded_variable, type(rounded_variable))
--- practices/average_grade_advanced.py
+++ practices/average_grade_advanced.py
@@ -1,6 +1,17 @@
-classes_total = int(input(f"How many classes are you taking?"))
+classes_total = input("How many classes are you taking? \n")
+num_total = int(classes_total)
+grades = []
 global period
-period = 1
+period = 0
 
-for int in classes_total:
-    grade = float(input(f"What is your grade in period {str(period)}?"))
+for i in range(num_total):
+    period += 1
+    grade = input("What is your grade in period " + str(period) + "? Don't put a percent sign in your answer. \n")
+    float_grade = float(grade)
+    grades.append(float_grade)
+    print(grades)
+
+average_grade = (sum(grades))/num_total
+rounded_average = round(average_grade, 2)
+print(f"Your average grade is {str(rounded_average)}")
+
