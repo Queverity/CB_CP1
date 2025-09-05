08891fd160f122f952f2cdb8c615157fee59c86d
Queverity <pryor.baird@ucas-edu.net>
2025-09-05 00:32:54 +0000

    finished idiot proof

--- practices/idiot_proof.py
+++ practices/idiot_proof.py
@@ -1,17 +1,33 @@
 # CB 1st Idiot Proof
+global phone
+global formatted_phone
+global gpa
+global float_gpa
+global rounded_gpa
 
-first_name = input("What is your first name? \n").capitalize().strip()
+first_name = input("What is your first name? \n").strip().capitalize()
 
-last_name = input("What is your last name? \n").capitalize().strip()
+last_name = input("What is your last name? \n").strip().capitalize()
 
-phone = input("What is your phone number?").strip()
+while True:
+    phone = input("What is your phone number?").strip()
+    if len(phone) == 10:
+        formatted_phone = phone[:3] + " " + phone[3:6] + " " + phone [6:]
+        break
+    else:
+        print("Invalid phone number")
+        continue
 
-gpa = input("What is your GPA?").strip()
-
-float_gpa = float(gpa)
-
-rounded_gpa = round(float_gpa,1)
+while True:
+    gpa = input("What is your non-weighted GPA?").strip()
+    float_gpa = float(gpa)
+    if float_gpa < 0:
+        print("Invalid GPA")
+        continue
+    elif float_gpa <= 4:
+        rounded_gpa = round(float_gpa,1)
+        break
 
 full_name = first_name + " " + last_name
 
-print(f"Name: {full_name} \nPhone Number: {phone} \nGPA: {rounded_gpa}")
+print(f"Name: {full_name} \nPhone Number: {formatted_phone} \nGPA: {rounded_gpa}")
