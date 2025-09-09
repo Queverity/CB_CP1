e2462a80e0301c387346c3c3d4594dcfbfec6e08
Queverity <pryor.baird@ucas-edu.net>
2025-09-09 09:14:30 -0600

    soup

---
+++ starters/f-strings.py
@@ -0,0 +1,7 @@
+name = "Alice"
+age = 25
+height = 1.65
+favorite_color = "blue"
+print(f"User info:\nName: {name}\nAge: {age}\nHeight in meters: {height}\nFavorite color: {favorite_color}")
+print(f"The person is {age} years old and {height} meters tall.")
+print(f"Favorite color: {favorite_color}\nColor code: {favorite_color[0]}")
