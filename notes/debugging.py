2166be07257bc15cce9fed44845524dd9af56c59
Queverity <pryor.baird@ucas-edu.net>
2025-09-25 09:53:04 -0600

    worked on debugging notes and fix game

---
+++ notes/debugging.py
@@ -0,0 +1,20 @@
+# CB 1st Debugging Notes
+
+# Syntax Error - Coding grammer error
+# print"vienna" <- bad
+print("Vienna") # good
+
+# Logic Error - Code runs, but it gives the wrong output\
+
+# numone = "2" <- should be int
+# numtwo = "5" <- should be int
+# print(numone + numtwo)
+
+numone = 2
+numtwo = 5
+print(numone + numtwo)
+
+# Have a well though out plan, and when bugs appear, step by step go through our logic
+# Rubber duck debugging: talking through a problem to someone else allows you to find issues easier
+
+# Runtime Error - happen when your code is running
