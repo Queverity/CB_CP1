d5f467ed87073f1831a379d77e797ee8e8683f78
Queverity <pryor.baird@ucas-edu.net>
2025-10-01 09:52:11 -0600

    started working on multiplacation table

--- practices/multiplacation_table.py
+++ practices/multiplacation_table.py
@@ -1,6 +1,26 @@
 # CB 1st Multiplacation Table
 
+row1 = ""
+row2 = ""
+row3 = ""
+row4 = ""
+row5 = ""
+row6 = ""
+row7 = ""
+row8 = ""
+row9 = ""
+row10 = ""
+row11 = ""
+row12 = ""
+
 column = 1
 row = 1
 
+for i in range(1,13):
+    product = str(column * row)
+    match column:
+        case 1:
+            row1 + (product + " ")
+            row += 1
 
+print(row1 + row2 + row3 + row4 + row5 + row6 + row7 + row8 + row9 + row10 + row11 + row12)
