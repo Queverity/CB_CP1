f87b7912861160dbb53802aa6a034d958252d3f5
Queverity <pryor.baird@ucas-edu.net>
2025-10-03 18:17:38 +0000

    fixed rock paper scissors

--- practices/rock_paper_scissors.py
+++ practices/rock_paper_scissors.py
@@ -1,6 +1,8 @@
 # CB 1st Rock Paper Scissors
 import random
 
+user_score = 0
+bot_score = 0
 print("Welcome to Rock Paper Scissors!")
 
 while True:
@@ -13,27 +15,34 @@ while True:
                     print("Bot played rock. It's a tie!")
                 case 2:
                     print("Bot played paper. You lost.")
+                    bot_score += 1
                 case 3:
                     print("Bot played scissors. You win!")
+                    user_score += 1
         case "Paper":
             match bot_answer:
                 case 1:
                     print("Bot played rock. You win!")
+                    user_score += 1
                 case 2:
                     print("Bot played paper. It's a tie!")
                 case 3:
                     print("Bot played scissors. You lost.")
+                    bot_score += 1
         case "Scissors":
             match bot_answer:
                 case 1:
                     print("Bot played rock. You lost.")
+                    bot_score += 1
                 case 2:
                     print("Bot played paper. You win!")
+                    user_score += 1
                 case 3:
                     print("Bot played scissors. It's a tie!")
         case _:
             print("Unexpected answer")
             continue
+    print(f"Player Score: {user_score}\n Bot Score: {bot_score}")
     play_again = input("Play again? Y/N").strip().capitalize()
     if play_again == "Y":
         continue
