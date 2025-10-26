4ba35a0335f5755b36125c4c12a7601717b7bbfa
Queverity <pryor.baird@ucas-edu.net>
2025-10-26 21:07:12 +0000

    finally finished this freaking "practice"

--- practices/combat.py
+++ practices/combat.py
@@ -1,82 +1,57 @@
 # CB 1st Combat Program
 import random
 import time
-fighter_stats = {"Class":"Fighter","Speed":9,"Offense":15,"Defense":16,"Health":100,"Attack":"D20"}
-rouge_stats = {"Class":"Rouge","Speed":15,"Offense":12,"Defense":14,"Health":80,"Attack":"D20"}
-mage_stats = {"Class":"Mage","Speed":12,"Offense":21,"Defense":12,"Health":60,"Attack":"D20"}
+fighter_stats = {"Class":"Fighter","Speed":9,"Offense":15,"Defense":13,"Health":100,"Attack":"D20"}
+rouge_stats = {"Class":"Rouge","Speed":15,"Offense":12,"Defense":12,"Health":80,"Attack":"D20"}
+mage_stats = {"Class":"Mage","Speed":12,"Offense":21,"Defense":11,"Health":60,"Attack":"D20"}
+admin_stats = {"Class":"God","Speed":1000,"Offense":1000,"Defense":1000,"Health":1000,"Attack":"D20"}
 monsters = ["Dire Wolf","Goblin","Job Application"]
 wolf_stats = {"Type":"Dire Wolf","Speed":12,"Offense":15,"Defense":10,"Health":50,"Attack":"D20"}
 goblin_stats = {"Type":"Goblin","Speed":10,"Offense":12,"Defense":13,"Health":75,"Attack":"D20"}
 job_stats = {"Type":"Job Application","Speed":14,"Offense":18,"Defense":10,"Health":90,"Attack":"D20"}
 turn = 0
 heals = 3
+lazy_fix = False
 monster_health = 0
 player_health = 0
 monster_type = ""
 
-def player_attack(monster_type,player_class,player_action):
-    if player_action == 1:
-        print("Rolling 1D20...")
+def player_attack(monster_type,player_class):
+    print("Rolling 1D20...")
+    time.sleep(1)
+    attack_roll = random.randint(1,20)
+    if attack_roll >= monster_type["Defense"]:
+        print(f"You rolled a(n) {attack_roll}. You hit!")
+        print("Rolling for damage...")
         time.sleep(1)
-        attack_roll = random.randint(1,20)
-        if attack_roll >= monster_type["Defense"]:
-            print(f"You rolled a(n) {attack_roll}. You hit!")
-            print("Rolling for damage...")
-            time.sleep(1)
-            player_damage = (player_class["Offense"]/3) + random.randint(1,6)
-            print(f"You dealt {player_damage} damage.")
-            return player_damage
-        elif attack_roll == 20:
-            print("You rolled a 20. Critical hit! You will deal double damage.")
-            print("Rolling for damage...")
-            time.sleep(1)
-            player_damage = (player_class["Offense"]/3) + random.randint(1,6)
-            player_damage = player_damage * 2
-            print(f"You dealt {player_damage} damage.")
-            return player_damage
-        else:
-            print("You missed.")
-            return 0
-    elif player_action == 2:
-        print("Rolling 1D20...")
+        player_damage = (player_class["Offense"]/3) + random.randint(1,6)
+        print(f"You dealt {player_damage} damage.")
+        return player_damage
+    elif attack_roll == 20:
+        print("You rolled a natural 20. Critical hit! You will deal double damage.")
+        print("Rolling for damage...")
         time.sleep(1)
-        attack_roll = random.randint(1,20)
-        if attack_roll >= monster_type:
-            print(f"You rolled a(n) {attack_roll}. You hit!")
-            print("Rolling for damage...")
-            time.sleep(1)
-            player_damage = (player_class["Offense"]/3) + random.randint(1,6)
-            self_damage = player_damage - 5
-            print(f"You dealt {player_damage} damage.")
-            print(f"You take {self_damage} damage.")
-            return player_damage * 2, self_damage
-        elif attack_roll == 20:
-            print("You rolled a 20. Critical hit! You will deal double damage.")
-            print("Rolling for damage...")
-            time.sleep(1)
-            player_damage = (player_class["Offense"]/3) + random.randint(1,6)
-            self_damage = player_damage - 5
-            player_damage = player_damage * 2
-            print(f"You dealt {player_damage} damage.")
-            print(f"You took {self_damage} damage.")
-            return player_damage * 2,self_damage
-        else:
-            print("You missed.")
-            return 0
+        player_damage = (player_class["Offense"]/3) + random.randint(1,6)
+        player_damage = player_damage * 2
+        print(f"You dealt {player_damage} damage.")
+        return player_damage
+    else:
+        print("You missed.")
+        return 0
     
 def monster_attack(player_class,monster_type):
     print("Rolling 1D20...")
     time.sleep(1)
-    attack_roll = random.randint(1,20)
+    attack_roll = random.randint(1,20) + 4
     if attack_roll >= player_class["Defense"]:
         print(f"The {monster_type["Type"]} rolled a(n) {attack_roll}. It hit.")
         print("Rolling for damage...")
         time.sleep(1)
         monster_damage = (monster_type["Offense"]/3) + random.randint(1,6)
-        print(f"The monster dealt {monster_damage}.")
+        print(f"The {monster_type["Type"]} dealt {monster_damage} damage.")
         return monster_damage
-    elif attack_roll == 20:
-        print(f"The {monster_type["Type"]} rolled a 20. Critical hit! it will deal double damage.")
+    elif attack_roll >= 20:
+        print(f"The {monster_type["Type"]} rolled a natural 20. Critical hit! it will deal double damage.")
         print("Rolling for damage...")
         time.sleep(1)
         monster_damage = (monster_type["Offense"]/3) + random.randint(1,6)
@@ -87,12 +62,22 @@ def monster_attack(player_class,monster_type):
         print(f"The {monster_type["Type"]} missed.")
         return 0
     
+def win_condition(player_health,monster_health):
+    if player_health <= 0:
+        print("You have been defeated.")
+        return 1
+    elif monster_health <= 0:
+        print("You have defeated your foe!")
+        return 2
+    else:
+        return 0
+    
 print("Combat Game thingy")
 
 while True:
-    print("Classes: \n1 Fighter\n2 Rouge\n3 Mage")
+    print("Classes: \n1 Fighter: A melee focused attacker.\n2 Rouge: A sneaky, speedy little gremlin.\n3 Mage: A magic focused glass cannon.\n4 Admin: Used for playtesting the game by the dev, has bonkers stats.")
     while True:
-        player_class = input("Pick your class! 1/2/3").strip()
+        player_class = input("Pick your class! 1/2/3/4").strip()
         match player_class:
             case "1":
                 player_class = fighter_stats
@@ -105,6 +90,10 @@ while True:
             case "3":
                 player_class = mage_stats
                 player_health = mage_stats["Health"]
+                break
+            case "4":
+                player_class = admin_stats
+                player_health = admin_stats["Health"]
                 break
             case _:
                 print("invalid anwswer")
@@ -140,13 +129,7 @@ while True:
         else:
             turn = 2
     while True:
-        if monster_health <= 0:
-            print(f"The {monster_type["Type"]} has been defeated!")
-            break
-        elif player_health <= 0:
-            print(f"You have been defeated by the {monster_type["Type"]}.")
-            break
-        else:
+        if lazy_fix == False:
             if turn == 1:
                 print("Player's Turn")
                 print("1. Basic attack")
@@ -163,19 +146,39 @@ while True:
                 print("3. Use healing potion (10 health, 3 uses max)")
                 print("4. Flee (may or may not work)") 
                 player_action = input("Which would you like to do? 1/2/3/4").strip()
-                if player_action == "1" or player_action == "2":
-                    monster_health -= player_attack(monster_type,player_class,player_action)
+                if player_action == "1":
+                    player_damage = player_attack(monster_type,player_class)
+                    monster_health -= player_damage
                     print(f"The {monster_type["Type"]} now has {monster_health} health left.")
+                    winner = win_condition(player_health,monster_health)
+                    if winner == 1 or winner == 2:
+                        break
+                    else:
+                        turn = 2
+                elif player_action == "2":
+                    player_damage = player_attack(monster_type,player_class) * 2
+                    monster_health -= player_damage
+                    player_health -= 6
+                    print(f"The {monster_type["Type"]} now has {monster_health} health left.")
+                    print(f"You took 6 damage.")
                     print(f"Player Health: {player_health}")
-                    turn = 2
+                    winner = win_condition(player_health,monster_health)
+                    if winner == 1 or winner == 2:
+                        break
+                    else:
+                        turn = 2
                 if player_action == "3":
                     while True:
                         if player_health == player_class["Health"]:
                             print("You are at max health and cannot heal.")
                             break
+                        elif heals == 0:
+                            print("You are out of health potions.")
+                            break
                         else:
                             player_health += 10
                             print(f"New health: {player_health}")
+                            heals -= 1
                             turn = 2
                             break
                 elif player_action == "4":
@@ -191,9 +194,13 @@ while True:
                         turn = 2
             if turn == 2:
                 print("Monster's turn")
-                monster_attack(player_class,monster_type)
+                player_health -= monster_attack(player_class,monster_type)
                 print(f"Player Health: {player_health}")
-                turn = 1
+                winner = win_condition(player_health,monster_health)
+                if winner == 1 or winner == 2:
+                    break
+                else:
+                    turn = 1
 
     go_again = input("Would you like to play again? Yes/No").strip().capitalize()
     if go_again == "Yes":
