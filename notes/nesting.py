61aa6fabfb3bb6c8e1cfac7dcb81a50f334a4f6b
Queverity <pryor.baird@ucas-edu.net>
2025-10-31 09:52:12 -0600

    started working on maxe generator

---
+++ notes/nesting.py
@@ -0,0 +1,8 @@
+# CB Nesting Notes 1st
+age = 17
+if age < 18:
+    print("You are not an adult.")
+    if age >= 17:
+        print("But you can drive!")
+    else:
+        print("You are an adult.")
---
+++ practices/maze_generator.py
@@ -0,0 +1,4 @@
+# CB 1st Maze Generator
+
+import turtle as t
+import random as r
