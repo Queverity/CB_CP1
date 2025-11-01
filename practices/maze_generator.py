120746f476051b20d3b85ef5c17fbb79b7fb5128
Queverity <pryor.baird@ucas-edu.net>
2025-11-01 17:51:12 +0000

    worked a bit on maze_generator

--- practices/maze_generator.py
+++ practices/maze_generator.py
@@ -1,4 +1,20 @@
 # CB 1st Maze Generator
 
 import turtle as t
-import random as r
+import random as ran
+
+def walls():
+    wall_drawer = t.Turtle()
+    wall_drawer.hideturtle()
+
+
+def initalize():
+    wn = t.Screen()
+    wn.bgcolor("lightblue")
+    wn.title("Maze Generator")
+    wn.setup(width=1000, height=1000)
+
+
+initalize()
+t.done()
+
