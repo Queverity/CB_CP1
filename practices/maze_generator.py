7267509c4c862227c4cdcc0ce96d6cbcad450933
Queverity <pryor.baird@ucas-edu.net>
2025-11-04 09:53:13 -0700

    worked on maze generator

--- practices/maze_generator.py
+++ practices/maze_generator.py
@@ -3,22 +3,56 @@
 import turtle as t
 import random as ran
 
-row_grid = []
-col_grid = []
+row_grid = [[],[],[],[],[],[]]
+col_grid = [[],[],[],[],[],[]]
 
-def is_solvable(row_grid,col_grid):
-    pass
+wn = t.Screen()
+wn.bgcolor("lightblue")
+wn.title("Maze Generator")
+wn.setup(width=1000, height=1000)
 
-def draw_walls():
+def solvable(row_grid, col_grid):
+
+    size = len(row_grid) - 1
+    visited = set()
+    stack = [(0,0)]
+
+    while stack:
+        x, y = stack.pop()
+        if x == size -1 and y == size -1:
+            return True
+        
+        if (x,y) in visited:
+            continue
+
+        visited.add((x,y))
+
+        if x < size -1 and col_grid[y][x+1] == 0:
+            stack.append((x+1,y))
+
+        if y < size -1 and row_grid[y+1][x] == 0:
+            stack.append((x,y+1))
+
+        if x > 0 and col_grid[y][x] == 0:
+            stack.append((x-1, y))
+
+        if y > 0 and row_grid[y][x] == 0:
+            stack.append((x,y-1))
+
+    return False
+
+
+def draw_walls(wn):
+    wn.tracer(0)
     wall_drawer = t.Turtle()
-    wall_drawer.color("black")
+    wall_drawer.color("yellow")
     wall_drawer.speed(10)
     wall_drawer.hideturtle()
     wall_drawer.penup()
     wall_drawer.goto(300,300)
     wall_drawer.pendown()
     wall_drawer.goto(300,-300)
-    wall_drawer.goto(33,-300)
+    wall_drawer.goto(100,-300)
     wall_drawer.penup()
     wall_drawer.goto(0,-300)
     wall_drawer.pendown()
@@ -26,46 +60,81 @@ def draw_walls():
     wall_drawer.goto(-300,300)
     wall_drawer.goto(0,300)
     wall_drawer.penup()
-    wall_drawer.goto(33,300)
+    wall_drawer.goto(100,300)
     wall_drawer.pendown()
     wall_drawer.goto(300,300)
+    wn.update()
+    wn.tracer(1)
 
 def generate_maze(row_grid,col_grid):
-    for _ in range(1,6):
-        rand_col = ran.randint(1,3)
-        rand_row = ran.randint(1,3)
-        if rand_col == 1:
-            col_grid.append(" ")
-        elif rand_col == 2:
-            col_grid.append("_")
-        elif rand_col == 3:
-            col_grid.append("|")
+    while True:
+        wn.tracer(0)
+        maze_drawer = t.Turtle()
+        maze_drawer.hideturtle()
+        maze_drawer.color("yellow")
+        maze_drawer.speed(10)
+        drawer_y_value = 300
+        drawer_x_value = -300
+        maze_drawer.penup()
+        maze_drawer.goto(drawer_x_value,drawer_y_value)
+        for i in range(0,6):
+            for i in range(0,6):
+                random_gen = ran.randint(0,1)
+                row_grid[i].append(random_gen)
 
-        if rand_row == 1:
-            row_grid.append(" ")
-        elif rand_col == 2:
-            row_grid.append("_")
-        elif rand_col == 3:
-            row_grid.append("|")
-    maze_drawer = t.Turtle()
-    maze_drawer.hideturtle()
-    maze_drawer.color("black")
-
-    for i in row_grid:
-        if i == " ":
+        for i in range(0,6):
+            for i in range(0,6):
+                random_gen = ran.randint(0,1)
+                col_grid[i].append(random_gen)
+            
+        for i in range(1,6):
             maze_drawer.penup()
+            drawer_y_value -= 100
+            maze_drawer.goto(-300,drawer_y_value)
+
+            for a in row_grid[i]:
+                if a == 0:
+                    maze_drawer.penup()
+                    maze_drawer.forward(100)
+                elif a == 1:
+                    maze_drawer.pendown()
+                    maze_drawer.forward(100)
+
+        maze_drawer.right(90)
+
+
+        for i in range(1,6):
+            maze_drawer.penup()
+            drawer_x_value += 100
+            maze_drawer.goto(drawer_x_value,300)
+            
+            for a in col_grid[i]:
+                if a == 0:
+                    maze_drawer.penup()
+                    maze_drawer.forward(100)
+                elif a == 1:
+                    maze_drawer.pendown()
+                    maze_drawer.forward(100)
+
+        if solvable():
+            break
+        else:
+            continue
+
+    
+
+
+        
+
             
 
 
 
-def initalize_screen():
-    wn = t.Screen()
-    wn.bgcolor("lightblue")
-    wn.title("Maze Generator")
-    wn.setup(width=1000, height=1000)
+
+   
 
 
-initalize_screen()
 draw_walls()
+generate_maze(row_grid,col_grid)
 t.done()
 
